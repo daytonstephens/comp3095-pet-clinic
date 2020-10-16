047bdd8d29afc66e3c05a3a342ee3a2f63c419ce
Dayton Stephens <dayton.stephens@georgebrown.ca>
2020-10-16 00:53:08 -0400

    Create Pet Clinic POJOs. Closes #2

---
+++ src/main/java/ca/gbc/comp3095/comp3095petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package ca.gbc.comp3095.comp3095petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/ca/gbc/comp3095/comp3095petclinic/model/Vet.java
@@ -0,0 +1,8 @@
+package ca.gbc.comp3095.comp3095petclinic.model;
+
+public class Vet extends Person {
+
+
+
+
+}
